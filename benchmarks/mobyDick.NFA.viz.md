2241e9785d67b38985f18415d8e429311f35b31f
Dave VanderWeele <weele.me@gmail.com>
2025-12-30 18:47:17 -0600

    shakespeare NFA SVG

---
+++ benchmarks/mobyDick.NFA.viz.md
@@ -0,0 +1,86 @@
+# Visualizing MASSIVE NFA State Machines for Shakespeare (66MB SVG) and Moby Dick (44MB SVG) Unique Words by Compiling from Adaptive Radix Tree (ART) to GraphViz
+
+After the initial release of my [JavaScript Adaptive Radix Tree project](https://dvanderweele.com/how-i-built-an-adaptive-radix-tree-compound-key-index-latin1-collation-regex-compiler-and-183kb-regex-pattern-for-the-complete-works-of-shakespeare-all-in-javascript) where we compiled massive regular expressions that match all the unique words in Moby Dick and the Complete Works of Shakespeare, I was mulling over diagram-as-code options for planning future projects.
+
+I happened upon [Mermaid.js State Diagrams](https://mermaid.js.org/syntax/stateDiagram.html). This would seem to be exactly what I was after! The label on the tin hits a lot of notes:
+
+* JavaScript ecosystem tool leads you to believe it will probably work on ARM64 platforms like Android Termux or Raspberry PI
+* Mildly snazzy website
+* Seemingly easy command file (.mmd) format syntax for targeting with our latest compilation procedure we're cooking up.
+* Multiple rendering target options include bitmap and thankfully vector format (SVG)
+
+As I was researching this Mermaid.js library, it struck me that these features could be a great candidate for possibly rendering visual representations of the non-deterministic finite automata that correspond to the huge regular expressions we compiled for unique words in Moby Dick and Shakespeare.
+
+So, I set out to do just that!
+
+And actually, I had a lot of problems with Mermaid — enough that I went elswhere. This isn't a hit piece by any means, but I was disappointed and so do want to note why I ended up abandoning the library.
+
+## Why I Abandoned Mermaid.js
+
+The first reason for my chagrin was that (as of this writing in late December 2025) the Mermaid-CLI tool is not usable on ARM64 processors. This means you can't use it in Termux on Android (huge bummer!), but it **also** means you can't even run this on a Raspberry PI.
+
+That's seriously disappointing. Let's say I don't want something resource- or time-intensive running on my phone; in that case I might want to throw it on the PI and check back later. But we cannot do this because ARM64 is not supported.
+
+Wow! I guess that means Mermaid is doing some really fancy and excellent things. So I plan on using my laptop for the build and meanwhile move on with development.
+
+The Mermaid documentation, while giving some syntax examples, does not appear to have actual BNF or other grammar. It's not the end of the world but it does require some more elbow grease to derive correct syntax in some cases. It did not prevent me from ultimately producing a valid `.mmd` file for mermaid-cli, but I did learn that for some reason it's not possible syntax-wise in this framework to define vertices in same command that you defined labelled transitions between them. 
+
+Finally, it was the moment of truth. I had compiled my `.mmd` file of commands from the intermediate result returned by my Adaptive Radix Tree's `toRegex` method. The resulting file was in the neighborhood of 3MB in size. I was ready to run the mermaid-cli to compile an epic SVG.
+
+I opened my gaming laptop and hit run.
+
+Before it would really run I had to research and create two config files to override various default limits (a timeout and a text limit) to give Mermaid a chance to actually run the file. But in the end, it did start running the file.
+
+It ran for a while.
+
+And then it ran some more.
+
+*And it kept running...*
+
+After some time it became clear to me it could run for an unrealistic amount of time. Some cursory research gave weight to that theory given the size of my `.mmd` file.
+
+While it was still running, I looked into alternate options.
+
+## Migrating to `GraphViz`
+
+GraphViz has a number of things going for it.
+
+* It works easily on ARM64 and is even installable through Termux `pkg` manager
+* Evidently it's faster for algorithmic reasons and also written in C
+* No messing around, they are literally just putting rule grammars in the documentation.
+* The syntax is quite similar to Mermaid but you evidently don't have to define your vertices separately from transitions.
+
+I modified my compilation script to target graphviz format and *voila*, I had a `.gv` command file ready. I plugged my phone in and kicked off this command in Termux: `dot -Tsvg mobyDick.gv -o mobyDick.svg`
+
+I laid down for a nap and upon waking a little more than an hour later, GraphViz had finished rendering the SVG... and Mermaid was still churning on my gaming laptop.
+
+We have a winner!
+
+The SVG for the NFA which recognizes all the words in Moby Dick is around 44MB in size. The aspect ratio when properly rendered is a wild 1×~307. That means if I physically printed it on a banner with a height of 1 foot, the banner would be longer than an American football field!
+
+The SVG for the Complete Works of Shakespeare NFA (compiled after Moby Dick) is about 66MB. Actually, it took more than 4 hours to complete in Termux (with full battery and mostly plugged in, too); since the file sizes are not even greater by a factor of two, this to me indicates even the GraphViz algorithms are superlinear somehow in terms of time complexity. However, clearly far faster than Mermaid. The aspect ratio for the Shakespeare SVG is about 1×328, or more than 1.25 American football fields if printed on an a 1-foot tall banner.
+
+And so it is hard to really get a proper view of these huge SVGs in any medium, but I found LibreOffice Draw as usual just works for this random need I had.
+
+These extreme aspect ratios are a testament to one of the chief advantages of trie data structures and also trie-style NFAs (trie regexes) for recognizing natural language words. That is, the height of the data structures, and therefore time required for membership tests, is limited to the length of words stored therein. As additional words are added, primarily the data structures keep growing horizontally.
+
+A bit about the compilation procedure and how it differs from what we've done so far with the Radix Tree project.
+
+## Details About Compiling a GraphViz NFA Definition from the Adaptive Radix Tree's `toRegex` Intermediate Result
+
+The intermediate result from the ART's `toRegex` is a tree-like data structure. The root is always a Group (Array-like with a boolean instance property called `optional`). Each Group has a layout like this: 
+
+1. A `ByteStack` — if you recall from the ART project overview this is a vector data structure for byte values.
+2. Zero or more entries after that where each can be either a `ByteStack` or another group.
+
+Based on the source representation of this data in the ART, we interpret differently the meaning of the boundaries in the array between `ByteStack` and Group instances depending on the order of the entries. In cases where a ByteStack preceeds a group, this is a place where a character in a regex pattern would be followed by a concatenation operator and then the start of a group. Conversely, in cases where a `ByteStack` succeeds a Group, this means the regex alternation operator would go between them. Finally, abutting `ByteStacks` naturally indicate the alternation operator between two strings.
+
+And so in the regular expression compilation adventures we determined, in a somewhat odd way, whether two neighboring entries in a Group were separated by an alternation or concatenation operator by way of a quick analysis of the last token pushed onto the output tokens array. 
+
+While okay for the purposes of compiling a regex pattern, a more robust approach was called for when compiling diagram-as-code commands for an NFA. 
+
+Before commencing my iterative depth-first traversal of the intermediate result to produce rules, I did a recursive traversal which reduced each Group array to another array of entries potentially smaller in size. Mainly, cases where a ByteStack had a Group concatenated after it were transformed to an instance of a single compound type.
+
+This makes managing the current prefix of the depth-first traversal tremendously easier and more accurate! Importantly, both Mermaid.js and GraphViz formats require unique identifier strings for each vertex defined. Naturally for that I use the prefix of the node/vertex.
+
+Anyway, for a closer look at the compilation logic check the `*.diagram.js` files in the ART project's `benchmarks/` directory.
---
+++ benchmarks/shakespeare.diagram.js
@@ -0,0 +1,263 @@
+import fs from "fs/promises"
+import path from "path"
+import {fileURLToPath} from 'url'
+import process from "node:process"
+import os from "os"
+import {ART,ByteStack,CapturingGroup,NonCapturingGroup} from "../art.js"
+import Latin1 from "../collations/Latin1/Latin1.js"
+
+const __filename = fileURLToPath(import.meta.url)
+
+const __dirname = path.dirname(__filename)
+
+const mobydick_file = path.join(__dirname,"shakespeare.txt")
+
+;(async ()=>{
+  const mobydick_raw = (await fs.readFile(
+    mobydick_file,
+    {encoding: "utf8"}
+  )).replaceAll("’","'").replaceAll("‘","'").replaceAll("œ","oe")
+  const wordPattern = /(?:[\s"\/_—,.!;:*&()”“?]+)?([^\s"\/_—,.!;:*&()”“?]+)/g
+  const words = [...mobydick_raw.matchAll(wordPattern)].map(w=>w[1])
+  const deduped = new ART() 
+  for(let i=0;i<words.length;i++){
+    const lword = Latin1.getL0SortKey(words[i].toLocaleLowerCase("en-US"))
+    deduped.insert(lword)
+  }
+  performance.mark("pre-expression-compile")
+  const rexpr = deduped.toRegex()
+  performance.mark("post-expression-compile")
+  // Depth-First Traversal of Intermediate Result
+  const prefix = new ByteStack()
+  class BranchSequence{
+    prefix
+    tails
+    idx
+    limit
+    constructor(prefix, tails){
+      this.prefix = prefix
+      this.tails = tails.reduce(
+        BranchSequence.interpret,
+        []
+      )
+      this.idx = 0
+      this.limit = this.tails.length
+    }
+    next(){
+      if(this.idx < this.limit){
+        const i = this.idx
+        this.idx++
+        return {
+          done: false,
+          value: this.tails[i]
+        }
+      }else{
+        return {
+          done: true
+        }
+      }
+    }
+    static interpret(acc,cur,idx,arr){
+      if(idx < arr.length-1){
+        if(cur instanceof ByteStack){
+          const nxt= arr[idx+1]
+          if(nxt instanceof ByteStack){
+            acc.push(cur)
+          }else{
+            acc.push(new BranchSequence(
+              cur, nxt
+            ))
+          }
+        }
+      }else{
+        if(cur instanceof ByteStack) acc.push(cur)
+      }
+      return acc
+    }
+  }
+  const stack = [
+    new BranchSequence(
+      {size:0},
+      rexpr.result
+    )
+  ]
+  const headerParts = ["digraph NFA {","  rankdir=TD;","  splines=true;","  overlap=false;","","  node [shape=circle, label=\"\", width=0.25, fixedsize=true];","  edge [fontsize=8];","  start [shape=point];","end [shape=doublecircle];"]
+  const transitions = []
+  const buildRule = (p,n,t) => {
+    const dv = p.pull()
+    const prefix = p.size == 0 ? "[*]" : [...(new Uint8Array(dv.buffer))].map(b=>(b).toString(16).padStart(2,"0")).join("")
+    const dst = t ? "[*]" : (n).toString(16).padStart(2,"0")
+    const rule = `${prefix}${prefix == "[*]" ? "" : ' : " "'} --> ${p.size > 0 ? prefix : ""}${dst} : ${dst == "[*]" ? "ε" : String.fromCharCode(n)}`
+    console.log(rule)
+    return rule
+  }
+  const limit = 300
+  let i = 0
+  while(stack.length > 0){
+    const tail = stack[stack.length-1]
+    const next = tail.next()
+    if(next.done){
+      const prefixSize = tail.prefix.size
+      for(let _ = 0; _ < prefixSize; _++) prefix.pop()
+      stack.pop()
+    } else {
+      const nextVal = next.value
+      if(nextVal instanceof ByteStack){
+        let prefixStr = [...(new Uint8Array(prefix.pull().buffer))].map(b=>(b).toString(16).padStart(2,"0")).join("") 
+        let lastPrefix = null
+        for(let ch of nextVal){
+          const nxtPfx = prefixStr + (ch).toString(16).padStart(2,"0")
+          const srcId = prefixStr == "" ? "start" : "x" + prefixStr
+          const dstId = nxtPfx
+          const rule = `  ${srcId} -> x${dstId} [label="${String.fromCharCode(ch)}"];`
+          lastPrefix = dstId
+          prefixStr = nxtPfx
+          transitions.push(rule)
+        }
+        transitions.push(`  x${lastPrefix} -> end [label="ε"];`)
+      }else{
+        // BranchSequence 
+        const nextVal = next.value
+        let prefixStr = [...(new Uint8Array(prefix.pull().buffer))].map(b=>(b).toString(16).padStart(2,"0")).join("") 
+        let lastPrefix = null
+        //console.log(121,nextVal)
+        //console.log(nextVal.constructor.name)
+        if(nextVal.prefix.size > 0){
+          for(let ch of nextVal.prefix){
+            prefix.push(ch)
+            const nxtPfx = prefixStr + (ch).toString(16).padStart(2,"0")
+            const srcId = prefixStr == "" ? "start" : "x"+prefixStr
+            const dstId = nxtPfx
+            const rule = `  ${srcId} -> x${dstId} [label="${String.fromCharCode(ch)}"];`
+            lastPrefix = dstId
+            prefixStr = nxtPfx
+            transitions.push(rule)
+          }
+        }
+        if(nextVal.tails.optional) tokens.push(`  x${lastPrefix} -> end [label="ε"];`)
+        stack.push(nextVal)
+      }
+    }
+      /**
+       * ABC
+       *      (
+       *          DEF
+       *          GHI
+       *                (
+       *                    JKL
+       *                    MNO
+       *                )
+       *          PQR
+       *      )
+       */
+  }
+  //console.log(tokens.join("\n"))
+  await fs.writeFile(path.join(__dirname,"shakespeare.gv"),[...headerParts, ...transitions, "}"].join("\n"),{encoding:"utf8"})
+  
+  /**
+   OLD
+  do {
+    const tail = stack[stack.length-1]
+    const next = tail.next()
+    if(i++ > limit) break
+    console.log("prefixlen",prefix.size,"stacklen",stack.length,stack.map(e=>e.constructor.name).join("|"))
+    if(next.done && lastGrp){
+      stack.pop()
+      if(next.popchar) prefix.pop()
+    } else {
+      let nextVal = next.value
+      if(
+        nextVal instanceof ByteStack
+      ){
+        for(let kb of nextVal){
+          console.log("\t"+String.fromCharCode(kb))
+          stack.push({
+            next(){
+              return {
+                done: true,
+                popchar: true
+              }
+            }
+          })
+          tokens.push(buildRule(prefix,kb,false))
+          prefix.push(kb)
+        }
+        lastGrp = false
+      }else{
+        // group
+        if(nextVal?.optional&&prefix.size>0) tokens.push(buildRule(prefix,-1,true))
+        stack.push(nextVal[Symbol.iterator]())
+        lastGrp = true
+      }
+    }
+  } while(stack.length > 0 && tokens.length < 300)
+  */
+  /*
+  performance.mark("pre-pattern-compile")
+  const rexStr = rexpr.serialize()
+  performance.mark("post-pattern-compile")
+  await fs.writeFile(path.join(__dirname,"mobyDick.regex.txt"),deduped.toRegex().serialize(),{encoding:"utf8"})
+  performance.mark("pre-RegExp-compile")
+  const rex = new RegExp("^"+rexStr+"$")
+  performance.mark("post-RegExp-compile")
+  let matches = 0 
+  let wc = 0
+  const OOR = new Map()
+  for(let i=0;i<words.length;i++){
+    const wrd = words[i].toLocaleLowerCase("en-US")
+    for(let ch of wrd){
+      const cp = ch.charCodeAt(0)
+      if(cp>255){
+        let e = OOR.get(cp)
+        if(!e){
+          e = new Set()
+          OOR.set(cp, e)
+        }
+        e.add(wrd)
+        break
+      }
+    }
+    const sk = Latin1.getL0SortKey(wrd)
+    const lword = String.fromCodePoint(...(sk.slice(0,sk.byteLength-1)))
+    console.log(i,"/",words.length, lword)
+    wc++
+    if(rex.test(lword)) matches++
+  }
+  console.log("matches:",matches,"words:",wc) 
+  console.log("expression compile duration (ms)", performance.measure("expression compile duration (ms)","pre-expression-compile","post-expression-compile").duration)
+  console.log("pattern compile duration (ms)", performance.measure("pattern compile duration (ms)","pre-pattern-compile","post-pattern-compile").duration)
+  console.log("RegExp compile duration (ms)", performance.measure("RegExp compile duration (ms)","pre-RegExp-compile","post-RegExp-compile").duration)
+  console.log("canary regex test failure, expect", false, "actual", rex.test("of course this isn't a word in moby dick!"))
+  console.log("words with out of range code points",[...OOR.entries()].map(a=>`${a[0]} § ${[...a[1].values()].join(" ‡ ")}`).join(" ||| "))
+*/
+  /**
+   * - Parse words list
+   * - Take memory baseline
+   * - Time insertions of all words into designated DS
+   * - Record Memory Usage
+   * - Time fullFwdRangeV of all words
+   * - Record word count vs deduped word count
+   * - Write stats into designated folder
+   */
+  /*
+node --expose-gc
+> process.memoryUsage();  // Initial usage
+{ rss: 19853312, heapTotal: 9751808, heapUsed: 4535648 }
+> gc();                   // Force a GC for the baseline.
+undefined
+> process.memoryUsage();  // Baseline memory usage.
+{ rss: 22269952, heapTotal: 11803648, heapUsed: 4530208 }
+> var a = new Array(1e7); // Allocate memory for 10m items in an array
+undefined
+> process.memoryUsage();  // Memory after allocating so many items
+{ rss: 102535168, heapTotal: 91823104, heapUsed: 85246576 }
+> a = null;               // Allow the array to be garbage-collected
+null
+> gc();                   // Force GC (requires node --expose-gc)
+undefined
+> process.memoryUsage();  // Memory usage after GC
+{ rss: 23293952, heapTotal: 11803648, heapUsed: 4528072 }
+> process.memoryUsage();  // Memory usage after idling
+{ rss: 23293952, heapTotal: 11803648, heapUsed: 4753376 }
+   */
+})()
